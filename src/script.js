78d60bb19d2c12c92c0fa66acac9e2c27d40fbe9
Marius Buharu <mariusbuharu18@gmail.com>
2023-01-04 12:46:10 +0200

    Added the api part

--- src/script.js
+++ src/script.js
@@ -143,35 +143,78 @@ tabsContainer.addEventListener('click', function (e) {
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
   clicked.classList.add('operations__tab--active');
-  console.log(`.operations__content--${clicked.dataset.tab}`);
   //Activate content area
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+//! API
 
 let searchQuery = 'pizza';
 const recipeIng = document.querySelector('.recipeIngredients');
+const recipeIng1 = document.querySelector('.recipeIngredients1');
+const recipeIng2 = document.querySelector('.recipeIngredients2');
+const recipeIng3 = document.querySelector('.recipeIngredients3');
+const recipeIng4 = document.querySelector('.recipeIngredients4');
 const recipeImg = document.querySelector('.recipeImg');
-const img = document.createElement('img');
+const recipeImg1 = document.querySelector('.recipeImg1');
+const recipeImg2 = document.querySelector('.recipeImg2');
+const recipeImg3 = document.querySelector('.recipeImg3');
+const recipeImg4 = document.querySelector('.recipeImg4');
+const recipeTitle = document.querySelector('.titleIng');
+const recipeTitle1 = document.querySelector('.titleIng1');
+const recipeTitle2 = document.querySelector('.titleIng2');
+const recipeTitle3 = document.querySelector('.titleIng3');
+const recipeTitle4 = document.querySelector('.titleIng4');
+const titlesEl = [
+  recipeTitle,
+  recipeTitle1,
+  recipeTitle2,
+  recipeTitle3,
+  recipeTitle4,
+];
+const imagesEl = [recipeImg, recipeImg1, recipeImg2, recipeImg3, recipeImg4];
+const recipesEl = [recipeIng, recipeIng1, recipeIng2, recipeIng3, recipeIng4];
+
 const APP_ID = 'f5f1ebde';
 const APP_key = '66960d0e2585033d807db12d058da6c1';
-async function fetchAPI() {
-  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
-  const response = await fetch(baseURL);
-  const data = await response.json();
 
-  console.log(data.hits[0]);
+//?Insert HTML
 
-  const ingredients = data.hits[0].recipe.ingredients;
-  const ingr = ingredients.forEach(ing => {
-    const html = `
-  <li>${ing.text}</li>
+const insertHtml = function (el, data) {
+  const html = `
+  <li>${data.text}</li>
   `;
-    recipeIng.insertAdjacentHTML('beforeend', html);
-    console.log(ing.text);
-  });
-  img.src = data.hits[0].recipe.image;
-  recipeImg.appendChild(img);
+  el.insertAdjacentHTML('beforeend', html);
+};
+//? Insert Images
+const insertImage = function (el, link) {
+  const img = document.createElement('img');
+  img.src = link;
+  img.classList.add('imageStyle');
+  el.insertAdjacentElement('beforeend', img);
+};
+
+async function fetchAPI() {
+  try {
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+    const response = await fetch(baseURL);
+    if (!response.ok) return new Error('Server could not be reached');
+    const data = await response.json();
+    const recipes = data.hits;
+
+    recipesEl.forEach((el, index) => {
+      const ingredients = recipes[index].recipe.ingredients;
+      const imgLink = recipes[index].recipe.image;
+      const title = recipes[index].recipe.label;
+      titlesEl[index].textContent = title;
+      ingredients.forEach(ing => {
+        insertHtml(el, ing);
+      });
+      insertImage(recipesEl[index], imgLink);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 fetchAPI();
